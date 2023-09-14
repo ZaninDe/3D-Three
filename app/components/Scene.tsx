30eb8f6c0a7bcc4698035592b717924c5ed79cdb
ZaninDe <gabriel.zaninde@gmail.com>
2023-09-14 10:49:55 -0300

    change textures by object

--- app/components/Scene.tsx
+++ app/components/Scene.tsx
@@ -1,19 +1,31 @@
 'use client'
 import { Stage } from "@react-three/drei"
 import { useLoader } from "@react-three/fiber"
+import { useEffect, useState } from "react"
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { TextureLoader, MeshBasicMaterial } from 'three';
 
-interface SceneProps  {
+interface SceneProps {
   onChangeObject: (object: any) => void
+  selectedObject: any
   texture: string
 }
 
-export const Scene = ({ onChangeObject, texture }: SceneProps) => {
+export const Scene = ({ onChangeObject, texture, selectedObject }: SceneProps) => {
   const { scene } = useLoader(GLTFLoader, './models/patriani_refatorado.glb')
 
-  const changeColor = (object: any) => {
-    console.log(object)
-  }
+  useEffect(() => {
+    console.log(selectedObject, texture)
+    if(selectedObject && texture !== '') {
+      const newTexture = new TextureLoader().load(texture);
+      const material = new MeshBasicMaterial({
+        map: newTexture,
+      });
+      selectedObject['material'] = material;
+      selectedObject['material'].needsUpdate = true;
+    }
+
+  }, [selectedObject, texture])
 
   return (
     <Stage adjustCamera intensity={1}>
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { Scene } from "./components/Scene";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { OBJECTS } from "./data/Objects";
+import  Image from 'next/image'
 
 export default function Home() {
   const [textures, setTextures] = useState<string[] | undefined>([])
@@ -15,21 +16,36 @@ export default function Home() {
       console.log(currentObject.name)
 
       const index = Object.keys(OBJECTS[0]).find(key => key === currentObject?.name);
+      console.log(currentObject)
 
-    if (index) 
+    if (index)  {
       setTextures(OBJECTS[0][index])
-    else
+      console.log(currentObject)
+    }
+      
+    else {
+      setCurrentObject(undefined)
       setTextures([])
+      setCurrentTexture('')
+    }
 
   }, [currentObject])
   return (
     <div className="w-screen h-screen">
       {
         currentObject && textures &&(
-          <div className="absolute top-10 left-10 flex gap-4">
+          <div className="absolute top-10 left-10 flex gap-4 z-10">
             {
               textures.map((item) => (
-                <div key={item} className="cursor-pointer" onClick={() => setCurrentTexture(item)}>{item}</div>
+                <div key={item} className="cursor-pointer" onClick={() => setCurrentTexture(item)}>
+                  <Image 
+                    src={item} 
+                    alt="texture image" 
+                    width={100} 
+                    height={100}
+                    className={`w-24 h-24 rounded-md border-2 ${item === currentTexture ? 'border-rose-600' : 'border-black'}`}
+                  />
+                </div>
               ))
             }
           </div>
@@ -37,7 +53,7 @@ export default function Home() {
       }
       <Canvas dpr={[2, 1]} camera={{ fov: 50 }}>
         <Suspense fallback={null}>
-          <Scene onChangeObject={setCurrentObject} texture={currentTexture}/>
+          <Scene onChangeObject={setCurrentObject} texture={currentTexture} selectedObject={currentObject}/>
         </Suspense>
         <OrbitControls enableZoom={true} enablePan={true} />
       </Canvas>
